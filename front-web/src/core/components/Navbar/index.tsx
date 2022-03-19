55d896e78d7610eead7a3c61daac3964090f3c42
Kaio Gomes <Kaio.e.m.gomes@gmail.com>
2022-03-19 18:48:55 -0300

    Add product listing with backend

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -6,7 +6,7 @@ const Navbar = () => (
   <nav className="row bg-primary main-nav">
     <div className="col-2">
       <Link to="/" className="nav-logo-text">
-        <h4>DS Catalog</h4>
+        <h4>KEM TECH</h4>
       </Link>
     </div>
     <div className="col-6 offset-2">
